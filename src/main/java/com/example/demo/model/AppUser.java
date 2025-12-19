a18a4c83bbafb18012381a9c27b9b302a8f8b410
bala-amypo <bala@amypo.in>
2025-12-19 08:40:01 +0000

    User commit

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -1,56 +1,44 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AppUser {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    @Column(nullable = false)
     private String password;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String fullName;
+
+    @NotNull
+    @Column(nullable = false)
     private String role;
-    public AppUser(Long id, String email, String password, String role) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-    
-    public AppUser() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    
-
-
 }

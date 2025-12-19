78a5fe6f6189040cc5490d84574fd96be2fe0989
bala-amypo <bala@amypo.in>
2025-12-19 08:43:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
-import com.example.demo.Service.AuthService;
+import com.example.demo.service.AuthService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
